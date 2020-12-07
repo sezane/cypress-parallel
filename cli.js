8de6fbe3cd2cb9018a5ff9ea7c5afecdf3dc123d
Bill Rawlinson <bill.rawlinson@uspsector.com>
2020-12-07 15:51:44 -0500

    fix format date function

--- cli.js
+++ cli.js
@@ -75,15 +75,17 @@ const getRandomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-const formatTime = function(timeMs) {
-  const seconds = Math.ceil(timeMs / 1000);
-  const sec = seconds % 60;
-  const min = Math.floor(seconds / 60);
-  let res = '';
+const formatTime = function(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  if (min) res += `${min}m `;
-  res += `${sec}s`;
-  return res;
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + "h " + minutes + "m " + seconds + "s " + milliseconds + " ms";
 };
 
 const start = () => {
